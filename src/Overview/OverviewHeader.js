2193f9fda63cfa69077530a35f22360c0d03c122
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-01 12:17:33 +0100

    logo

--- src/Overview/OverviewHeader.js
+++ src/Overview/OverviewHeader.js
@@ -2,47 +2,60 @@ import { useContext } from "react";
 import { UserContext } from "../Providers/UserProvider.js";
 import Dropdown from "react-bootstrap/Dropdown";
 import { FaRegUserCircle } from "react-icons/fa";
+import logo from "../Logo/logo.png";
 
 function Header() {
   const { userList, loggedInUser, setLoggedInUser } = useContext(UserContext);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        width: "100vw",
-        paddingTop: "30px",
-        paddingRight: "40px",
-      }}
-    >
-      <Dropdown>
-        <Dropdown.Toggle
-          as="div"
-          variant="success"
-          id="dropdown-basic"
-          style={{
-            border: "green",
-            padding: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <FaRegUserCircle size={60} />
-        </Dropdown.Toggle>
+    <div>
+      <div
+        className="userIcon"
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          width: "100vw",
+          paddingTop: "30px",
+          paddingRight: "40px",
+        }}
+      >
+        <Dropdown>
+          <Dropdown.Toggle
+            as="div"
+            variant="success"
+            id="dropdown-basic"
+            style={{
+              padding: 0,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <FaRegUserCircle size={60} />
+          </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          {userList.map((user) => (
-            <Dropdown.Item
-              key={user.id}
-              onClick={() => setLoggedInUser(user.id)}
-            >
-              {user.name} {user.id === loggedInUser ? "(current)" : ""}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
+          <Dropdown.Menu>
+            {userList.map((user) => (
+              <Dropdown.Item
+                key={user.id}
+                onClick={() => setLoggedInUser(user.id)}
+              >
+                {user.name} {user.id === loggedInUser ? "(current)" : ""}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      <div className="logo">
+        <img
+          src={logo}
+          alt="Logo"
+          style={{
+            height: "60px",
+          }}
+        />
+      </div>
     </div>
   );
 }
